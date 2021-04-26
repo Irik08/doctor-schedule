543de1cab9ca637498fbc42b81da3b5ee2bc595e
DNovikov <drnovikovds@gmail.com>
2021-04-26 19:32:16 +0300

    Create Application

---
+++ DoctorSchedule/DoctorSchedule/DoctorRepository.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DoctorSchedule
+{
+    public class DoctorRepository
+    {
+        private List<Doctor> _doctorList;
+        public void Add(Doctor doc) 
+        {
+            _doctorList.Add(doc);
+        }
+    }
+}
---
+++ DoctorSchedule/DoctorSchedule/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DoctorSchedule
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
